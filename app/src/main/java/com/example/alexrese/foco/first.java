734d7baeab5056968030c7676ad683d3d52e2ae2
alexrese <alexrese@outlook.com>
2019-04-25 18:24:45 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/alexrese/foco/first.java
@@ -0,0 +1,57 @@
+package com.example.alexrese.foco;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class first extends AppCompatActivity {
+    String msg = "Android : ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Log.d(msg, "The onCreate() event");
+    }
+
+
+    public void abrirSegundaActivity(View view){
+        startActivity(new Intent(getBaseContext(), second.class));
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+}
